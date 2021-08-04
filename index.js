8137d8316435b88b3c5a4cc99dedf11991d44a9c
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-04 23:26:05 +0100

    Added Pig Latin challenge

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ function save(){
  save()
 
 
+
+
     
 
     
---
+++ practice1.js
@@ -0,0 +1,23 @@
+ /*
+    Javascript  Setup:
+
+    ** Welcome Messege setup
+    1. create a variable welcome-el and call id welcome-El
+    2. create variable name and set to name1, stGreating and set to "Welcome back"
+    3. set welcome-El innerText or textContant = stGreeting and concat name1 and then 
+         add emoji
+
+    ** Increment count**
+    5. create count variable and set to 0;
+    6. create countEl and set to document to count-El id;
+    7. create increment function that increments count and set to count-El.innert,
+        then return count. Evoke increment function;
+
+    ** Save Fearture **
+    8. create saveEl veriable and set to documet with  save-El id    
+    9. create save count function
+    10. create saveCount veriable and set to count 
+    11. saveEl.textContent  and increment savedCount.
+    12. finally set the count to zero after saved and then evoke save function.
+
+*/
