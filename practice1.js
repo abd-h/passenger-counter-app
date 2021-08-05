3007d0979b2044e3816659ef3bcf888869cbd62f
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-05 18:14:50 +0100

    Added practices of js

--- practice1.js
+++ practice1.js
@@ -1,4 +1,38 @@
- /*
+// Welcome Message
+let welcomeEl = document.getElementById("welcome-el");
+let name2 = "Joe";
+let greeting2 = "Welcome Back";
+welcomeEl.textContent = `${greeting2} ${name2}`;
+welcomeEl.textContent += "  👋🏽"
+console.log(welcomeEl);
+//..................................
+
+// Icrementing
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+function increment(){
+    count++;
+    countEl.textContent = count;
+    return count
+}
+increment();
+console.log(count);
+//......................................................
+
+//Save feature
+
+let saveEl = document.getElementById("save-el");
+function save(){
+    let saveCount = count + "-";
+    saveEl.textContent += saveCount;
+   countEl.textContent = 0; 
+   count = 0;
+}
+save();
+console.log(count);
+/*
     Javascript  Setup:
 
     ** Welcome Messege setup
@@ -14,10 +48,10 @@
         then return count. Evoke increment function;
 
     ** Save Fearture **
-    8. create saveEl veriable and set to documet with  save-El id    
+    8. create saveEl veriable and set to documet with  save-el id    
     9. create save count function
     10. create saveCount veriable and set to count 
     11. saveEl.textContent  and increment savedCount.
-    12. finally set the count to zero after saved and then evoke save function.
+    12. finally set the count to zero after save and then evoke save function.
 
 */
